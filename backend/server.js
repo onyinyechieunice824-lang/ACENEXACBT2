db35572a250cc209c3b5fea62d3276cc803621a8
onyinyechieunice824-lang <onyinyechieunice824@gmail.com>
2026-01-06 22:16:27 +0100

    Update allowed origins for CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // --------------------- CORS ---------------------
 const allowedOrigins = [
-  'https://acenexacbt-2.vercel.app',
+  'https://acenexacb-tupdate.vercel.app',
   'https://acenexacbt.onrender.com',
   'http://localhost:5173',
   'http://localhost:3000'
